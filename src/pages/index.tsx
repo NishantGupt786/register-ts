b7db92ba31e2ecb2a19e19fea200ad1892f17171
NishantGupt786 <nishantgupta2325@gmail.com>
2023-10-04 17:20:01 +0530

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,266 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useFormik } from "formik";
+import { z } from "zod";
+import { toFormikValidationSchema } from "zod-formik-adapter";
+import ReactFlagsSelect from "react-flags-select";
+import { useState } from "react";
 
 export default function Home() {
+  const [country, setCountry] = useState<string>("");
+  const userSchema = z
+    .object({
+      firstName: z
+        .string({
+          required_error: "Required",
+          invalid_type_error: "First name must be a string",
+        })
+        .min(2, "First name must have min 2 chars")
+        .max(20, "First name must have max 20 chars"),
+      lastName: z
+        .string({
+          required_error: "Required",
+          invalid_type_error: "Last name must be a string",
+        })
+        .max(20, "Last name must have max 20 chars"),
+      email: z
+        .string({
+          required_error: "Required",
+          invalid_type_error: "Email must be a string",
+        })
+        .email("Enter a valid email"),
+      password: z
+        .string({
+          required_error: "Required",
+          invalid_type_error: "Password must be a string",
+        })
+        .regex(
+          /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+          "Password should contain atleast 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 special character"
+        ),
+      confirmPassword: z.string({
+        required_error: "Required",
+        invalid_type_error: "Confirm password must be a string",
+      }),
+      country: z.string({
+        required_error: "Required",
+        invalid_type_error: "Country must be a string",
+      }),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    });
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      country: country,
+    },
+    validationSchema: toFormikValidationSchema(userSchema),
+    validateOnChange: true,
+    onSubmit: async (values) => {
+      const send = {
+        firstName: values.firstName,
+        lastName: values.lastName,
+        email: values.email,
+        password: values.password,
+        coutry: values.country,
+      };
+      console.log(send);
+    },
+  });
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    formik;
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="mx-10">
+      <form onSubmit={handleSubmit}>
+        <div className="pt-1">
+          <div className="mt-6 grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="firstName"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                First name
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  autoComplete="firstName"
+                  value={values.firstName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="First Name"
+                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 px-4 focus:ring-2 focus:ring-inset focus:ring-[#37ABBC] sm:text-sm sm:leading-6 
+                      ${
+                        touched.firstName && errors.firstName
+                          ? "ring-2 ring-inset ring-red-500"
+                          : ""
+                      }`}
+                />
+
+                <span className="text-sm text-red-500">
+                  {touched.firstName && errors.firstName}
+                </span>
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="lastName"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Last name
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                  autoComplete="lastName"
+                  value={values.lastName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="Last Name"
+                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 px-4 focus:ring-2 focus:ring-inset focus:ring-[#37ABBC] sm:text-sm sm:leading-6
+                      ${
+                        touched.lastName && errors.lastName
+                          ? "ring-2 ring-inset ring-red-500"
+                          : ""
+                      }`}
+                />
+                <span className="text-sm text-red-500">
+                  {touched.lastName && errors.lastName}
+                </span>
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Email
+              </label>
+              <div className="mt-2">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="Email"
+                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 px-4 focus:ring-2 focus:ring-inset focus:ring-[#37ABBC] sm:text-sm sm:leading-6
+                      ${
+                        touched.email && errors.email
+                          ? "ring-2 ring-inset ring-red-500"
+                          : ""
+                      }`}
+                />
+                <span className="text-sm text-red-500">
+                  {touched.email && errors.email}
+                </span>
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  autoComplete="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="Password"
+                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 px-4 focus:ring-2 focus:ring-inset focus:ring-[#37ABBC] sm:text-sm sm:leading-6 ${
+                    touched.password && errors.password
+                      ? "ring-2 ring-inset ring-red-500"
+                      : ""
+                  } `}
+                />
+                <span className="text-sm text-red-500">
+                  {touched.password && errors.password}
+                </span>
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="confirmPassword"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Confirm Password
+              </label>
+              <div className="mt-2">
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  value={values.confirmPassword}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="Confirm Password"
+                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 px-4 focus:ring-2 focus:ring-inset focus:ring-[#37ABBC] sm:text-sm sm:leading-6
+                      ${
+                        touched.confirmPassword && errors.confirmPassword
+                          ? "ring-2 ring-inset ring-red-500"
+                          : ""
+                      }`}
+                />
+                <span className="text-sm text-red-500">
+                  {touched.confirmPassword && errors.confirmPassword}
+                </span>
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="country"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Country
+              </label>
+              <div className="">
+                <ReactFlagsSelect
+                  selected={country}
+                  onSelect={(countryCode) => {
+                    setCountry(countryCode);
+                    formik.handleChange("country")(countryCode);
+                  }}
+                  placeholder="Select Country"
+                  searchable
+                  searchPlaceholder="Search countries"
+                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#37ABBC] sm:text-sm sm:leading-6
+                      ${
+                        touched.country && errors.country
+                          ? "ring-2 ring-inset ring-red-500"
+                          : ""
+                      }`}
+                />
+
+                <span className="text-sm text-red-500">
+                  {touched.country && errors.country}
+                </span>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="rounded-md text-base bg-[#37ABBC] text-white hover:bg-[#288391] px-2 py-2 font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#37ABBC]"
+            >
+              Register
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </form>
     </main>
-  )
+  );
 }
